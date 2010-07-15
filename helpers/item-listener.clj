b72d895cc403d4840b43b28494bf6276ca232a7d
abhijith <abhijithg@gmail.com>
2010-07-15 19:21:48 +0530

    item listener helper added

---
+++ helpers/item-listener.clj
@@ -0,0 +1,17 @@
+(ns swing.helpers.item-listener
+  (:import (java.awt.event ItemListener ItemEvent)))
+
+(defn add-item-listener
+  [component f & args]
+  (let [listener (proxy [ItemListener] []
+		   (itemStateChanged [event]
+		     (apply f event args))) ]
+    (.addItemListener component listener)))
+
+(defn item-selected?
+  [event]
+  (= (.getStateChange event) ItemEvent/SELECTED))
+
+(defn item-deselected?
+  [event]
+  (= (.getStateChange event) ItemEvent/DESELECTED))
